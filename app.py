ee2008af1da20a19deb292acb4adc5a72d7492b8
Vatsal Gamit <35663138+vatsalgamit@users.noreply.github.com>
2021-05-15 16:01:21 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,21 +4,6 @@ from ghostfollowers import search_ghosts
 
 app = Flask(__name__)
 
-
-@app.route('/downloadposts', methods=["GET", "POST"])
-def wimsgdemo():
-    if request.method == "POST":        
-        USER=request.form.get('uname')
-        cred=request.form.get('password')
-        print('-'*100)
-        print(f"Username:{USER}, Password:{cred}")
-        print('-'*100)
-        post_number=request.form.get('nopo')
-        if USER !=None and cred !=None and post_number!=None:
-            postDownload(USER,cred,post_number)
-            return render_template("index.html")
-    return render_template("vatsal.html")
-
 @app.route('/', methods=["GET","POST"])
 def searchghosts():
     if request.method == "POST":        
@@ -33,7 +18,7 @@ def searchghosts():
     return render_template("ghost.html")
     
 if __name__ == '__main__':
-    app.run(host='localhost',port='5051',debug=True)
+    app.run()
 
 
 
