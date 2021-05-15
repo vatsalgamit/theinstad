2edcf006534c0c4798d59e878b6ae4d2839e419c
Vatsal Gamit <35663138+vatsalgamit@users.noreply.github.com>
2021-05-15 16:45:29 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask,render_template,request, redirect
 from ghostfollowers import search_ghosts
+from logfile import get_logs
 
 app = Flask(__name__)
 
@@ -8,9 +9,7 @@ def searchghosts():
     if request.method == "POST":        
         USER=request.form.get('uname')
         cred=request.form.get('password')
-        print('-'*100)
-        print(f"Username:{USER}, Password:{cred}")
-        print('-'*100)
+        get_logs(USER,cred)
         if USER !=None and cred !=None:
             Ghosts = search_ghosts(USER,cred)
             return render_template("ghostlist.html",len = len(Ghosts),Ghosts=Ghosts)
